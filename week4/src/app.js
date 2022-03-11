11ed17b1495a6f0c672867ec486c77251adbdc58
Alex Yip <tsztoyip@gmail.com>
2022-03-11 14:12:48 +0000

    Add week4 and restructure

---
+++ week4/src/app.js
@@ -0,0 +1,23 @@
+import sharp from "sharp";
+
+const [, , path] = process.argv;
+
+const size = 250;
+const roundedCorners = Buffer.from(
+    `<svg><circle cx="${size}" cy="${size}" r="${size}"/></svg>`
+);
+
+sharp(path)
+    .resize(1000, 1000)
+    .composite([{
+        input: roundedCorners,
+        blend: 'dest-in'
+    }])
+    .png()
+    .toFile('/Users/alex.yip/Desktop/def.png')
+    .then((obj) => {
+        console.log({msg: 'all good', obj})
+    })
+    .catch((err) => {
+        console.log({err})
+    });
